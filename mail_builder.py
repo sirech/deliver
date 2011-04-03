5bfde7ecf092817c404b2577e4b0d123180808b9
Mario Fernandez <mario.fernandez@hceris.com>
2011-04-03 15:42:42 +0200

    fix bug with multiple recievers, added cfg json file, removed mail_builder

--- mail_builder.py
+++
@@ -1,7 +0,0 @@
-from email.mime.text import MIMEText
-
-def create_msg(sender, subject, content):
-    msg = MIMEText(content)
-    msg['From'] = sender
-    msg['Subject'] = subject
-    return msg
--- send.py
+++ send.py
@@ -1,22 +1,35 @@
 import smtplib
 import json
-from mail_builder import create_msg
+
+from email.mime.text import MIMEText
 
 class Sender:
 
     def __init__(self):
-        self._credentials = self.load_credentials('credentials.json')
+        self._credentials = self.load_json('credentials.json')
+        self._cfg = self.load_json('configuration.json')
 
     def send(self, content, recipients):
-        msg = create_msg(self.get_address(), 'test', 'some message')
-        s = smtplib.SMTP('localhost')
+        msg = self.create_msg('test', 'some message')
         for recipient in recipients:
+            s = smtplib.SMTP('localhost')
             msg['To'] = recipient
             s.sendmail(self.get_address(), recipient, msg.as_string())
             s.quit()
 
-    def load_credentials(self, filename):
+    def load_json(self, filename):
         return json.load(open(filename))
 
     def get_address(self):
         return self._credentials['sender']
+
+    def create_msg(self, subject, content):
+        msg = MIMEText(content)
+        msg['From'] = self.get_address()
+        msg['Subject'] = self.prepare_subject(subject)
+        return msg
+
+    def prepare_subject(self, subject):
+        if self._cfg['subject_prefix'] in subject:
+            return subject
+        return self._cfg['subject_prefix'] + ' ' + subject
