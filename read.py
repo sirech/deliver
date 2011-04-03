d6667224e4ccf438001873668b100ab5be1d5f57
Mario Fernandez <mario.fernandez@hceris.com>
2011-04-03 17:39:53 +0200

    add module to receive mail

---
+++ read.py
@@ -0,0 +1,36 @@
+import poplib
+import email
+
+class Reader:
+
+    def __init__(self):
+        self._creds = self.load_json('credentials.json')
+        self._cfg = self.load_json('configuration.json')
+
+
+    def connect(self):
+        self._s = poplib.POP3(self._creds['server'])
+        self.check(self._s.user(self._creds['sender']))
+        self.check(self._s.pass_(self._creds['password']))
+
+    def disconnect(self):
+        self.check(self._s.quit())
+
+    def new_messages(self):
+        st, msg_list, _ = self._s.list()
+        self.check(st)
+        return [msg.split('')[0] for msg in msg_list]
+
+    def get(self, id):
+        st, lines, _ = self._s.retr(id)
+        self.check(st)
+        return email.message_from_string('\n'.join(lines))
+
+    def delete(self, id):
+        self.check(self._s.dele(id))
+
+    def check(self, st):
+        if not st.startswith('+OK') and 'debug' in self._creds:
+            from send import Sender
+            snd = Sender()
+            snd.send('DEBUG', st, self._creds['debug'])
--- send.py
+++ send.py
@@ -6,13 +6,13 @@ from email.mime.text import MIMEText
 class Sender:
 
     def __init__(self):
-        self._credentials = self.load_json('credentials.json')
+        self._creds = self.load_json('credentials.json')
         self._cfg = self.load_json('configuration.json')
 
-    def send(self, content, recipients):
+    def send(self, subject, content, *recipients):
         for recipient in recipients:
-            s = smtplib.SMTP('localhost')
-            msg = self.create_msg('test', 'some message')
+            s = smtplib.SMTP(self._creds['server'])
+            msg = self.create_msg(subject, content)
             msg['To'] = recipient
             s.sendmail(self.get_address(), recipient, msg.as_string())
             s.quit()
@@ -21,7 +21,7 @@ class Sender:
         return json.load(open(filename))
 
     def get_address(self):
-        return self._credentials['sender']
+        return self._creds['sender']
 
     def create_msg(self, subject, content):
         msg = MIMEText(content)
