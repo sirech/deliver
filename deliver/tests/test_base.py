8243e0cf6e34164ba81279fbef780d874af22786
Mario Fernandez <mario.fernandez@hceris.com>
2011-04-12 23:21:02 +0200

    change config for tests to a file

--- deliver/tests/test_base.py
+++ deliver/tests/test_base.py
@@ -3,14 +3,5 @@ import unittest
 class BaseTest(unittest.TestCase):
 
     def setUp(self):
-        self.config = {
-            'sender' : 'test@test.com',
-            'smtp_server' : 'localhost',
-            'pop_server' : 'mail.test.com',
-            'password' : 'abcdef',
-            'subject_prefix' : u'[Test]',
-            'introductions' : [u'says', u'tells'],
-            'quotes' : [u'No quote', u'No quote 2'],
-            'real_name' : u'Test',
-            'members' : 'members.json.example'
-            }
+        from test_data.test_config import py
+        self.config = py
---
+++ test_data/test_config.py
@@ -0,0 +1,12 @@
+# -*- coding: latin-1 -*-
+py = {}
+
+py['sender'] = 'test@test.com'
+py['smtp_server'] = 'localhost'
+py['pop_server'] = 'mail.test.com'
+py['password'] = 'abcdef'
+py['subject_prefix'] = '[Test]'
+py['introductions'] = ['says', 'tells']
+py['quotes'] = ['No quote', 'No quote 2']
+py['real_name'] = 'Test'
+py['members'] = 'members.json.example'
