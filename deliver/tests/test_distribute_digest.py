456a7ae15b041c2345efbd60ab0159d1aedf8d2a
Mario Fernandez <mario.fernandez@friendscout24.de>
2011-04-20 16:27:36 +0200

    add more tests for update digest

--- deliver/tests/test_distribute_digest.py
+++ deliver/tests/test_distribute_digest.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-from test_base import BaseTest, load_msg, get_msg, archive_msg
+from test_base import BaseTest, load_msg, get_msg, archive_msg, get_digests
 from mock import Mock
+from datetime import datetime, timedelta
 
 from deliver.distribute import OfflineDistributor
 from deliver.send import Sender
@@ -15,9 +16,12 @@ class OfflineDistributeTest(BaseTest):
         self.store = self.distributor._store
         self.msg = load_msg('sample')
 
-    def test_update_nothing_new(self):
+    def _test_update_no_msgs(self):
         self.assertFalse(self.distributor.update())
         self.assertEqual(self.sender.send.call_count, 0)
+
+    def test_update_nothing_new(self):
+        self._test_update_no_msgs()
 
     def _test_update(self, fileList, *addresses):
         for f in fileList:
@@ -40,3 +44,18 @@ class OfflineDistributeTest(BaseTest):
 
     def test_update_multiple_msg_multiple_addr(self):
         self._test_update(['sample5', 'sample7' ], u'test@mail.com',  u'external@mail.com')
+
+    def test_update_old_msgs(self):
+        address = u'test@mail.com'
+        archive_msg(self.store, 'sample8', address)
+        get_digests(self.store, address)[0].scheduled_at = datetime.now() - timedelta(days=100)
+        self.store._db.session.flush()
+
+        self._test_update_no_msgs()
+
+    def test_update_done_msgs(self):
+        address = u'test@mail.com'
+        archive_msg(self.store, 'sample9', address)
+        self.store.mark_digest_as_sent(address)
+
+        self._test_update_no_msgs()
