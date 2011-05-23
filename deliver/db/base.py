d42f067de8e963e7ce2f892215b6ff50b2d9a7f0
Mario Fernandez <mario.fernandez@hceris.com>
2011-05-23 19:59:15 +0200

    create indexes for tables

--- deliver/db/base.py
+++ deliver/db/base.py
@@ -12,7 +12,7 @@ class BaseDBWrapper(object):
         metadata = MetaData()
         metadata.bind = self.engine
         self.messages = self._create_table('messages', metadata,
-                                           Column('id', String(192), primary_key=True),
+                                           Column('id', String(192), index=True, primary_key=True),
                                            Column('content', Text, nullable=False),
                                            Column('received_at', DateTime, nullable=False),
                                            Column('sent_at', DateTime))
@@ -20,7 +20,7 @@ class BaseDBWrapper(object):
         self.digests = self._create_table('digests', metadata,
                                           Column('msg_id', String(192), ForeignKey('messages.id', ondelete='cascade'),
                                                  primary_key=True),
-                                          Column('send_to', String(192), primary_key=True),
+                                          Column('send_to', String(192), index=True, primary_key=True),
                                           Column('scheduled_at', DateTime, nullable=False),
                                           Column('sent_at', DateTime))
 
